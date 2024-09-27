b9f9df84cc25f3e750b1bc18bdbeafb13a5502fc
Zharif-Mahmud04 <zharifmahmud2011@gmail.com>
2024-09-27 09:11:17 +0600

    Create Firebase.config.jsx

---
+++ Firebase.config.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBSDoHm0rCSEA44TVs--CMi6iXUxAooTIg",
+  authDomain: "agriwavesunveil.firebaseapp.com",
+  projectId: "agriwavesunveil",
+  storageBucket: "agriwavesunveil.appspot.com",
+  messagingSenderId: "693004342575",
+  appId: "1:693004342575:web:9a646247f59d498ad83dda",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig
